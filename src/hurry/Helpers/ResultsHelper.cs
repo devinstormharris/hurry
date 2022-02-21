e8e3577a54ce276f6d460709734940450cd0e755
Devin Harris <49620674+DevinStormHarris@users.noreply.github.com>
2022-02-21 18:48:04 -0500

    add todo to fix wpm calculation

--- src/hurry/Helpers/ResultsHelper.cs
+++ src/hurry/Helpers/ResultsHelper.cs
@@ -7,19 +7,11 @@ public static class ResultsHelper
     public static int CalculateWpm(Test test)
     {
         try
-        {
-            // to normalize results, we are considering 1 "word" to equal 5 characters, so we determine
-            // words entered by dividing total characters by 5
-
+        { // TODO: calculate wpm accurately
             var wordCount = test.UserInput.Length / 5L;
-
-            // we first calculate words per second
-
             var wordsPerSecond = wordCount / test.Seconds;
-
-            // then we multiply words per second by 60 to get words per minute (wpm)
-
             var results = (int)wordsPerSecond * 60;
+            
             return results;
         }
         catch (Exception)
