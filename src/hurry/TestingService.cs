0083cbdf61a494d36621a646639943c4c163b69e
Devin Harris <49620674+DevinStormHarris@users.noreply.github.com>
2022-02-21 13:44:07 -0500

    implement models in TestingService.cs

---
+++ src/hurry/TestingService.cs
@@ -0,0 +1,24 @@
+using hurry.Helpers;
+using hurry.Models;
+
+namespace hurry;
+
+public class TestingService
+{
+    public Results Start()
+    {
+        var prompt = PromptHelper.GetPrompt();
+        var test = new Test(prompt);
+
+        TimeHelper.Start();
+        Console.WriteLine(test.ToString());
+
+        var input = Console.ReadLine();
+        TimeHelper.Stop();
+
+        var wpm = ScoreHelper.GetScore(input!);
+        var results = new Results(wpm);
+
+        return results;
+    }
+}
