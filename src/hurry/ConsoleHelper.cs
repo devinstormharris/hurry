f7308839ea4f4d894d95f8a088e0edb8abc38aca
Devin Harris <49620674+devinstormharris@users.noreply.github.com>
2022-03-02 20:57:05 -0500

    Update Program with better DI and a ConsoleHelper

---
+++ src/hurry/ConsoleHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hurry
+{
+    public static class ConsoleHelper
+    {
+        public static void Greet()
+        {
+            Console.WriteLine("Welcome to hurry, a typing test where you want to hurry.");
+            Console.WriteLine("You will be given one minute to type out the prompt that you will see.");
+            Console.WriteLine("If you want to end the program early, you can type 'quit'");
+        }
+
+        public static async Task StartCountdown()
+        {
+            for (var i = 3; i > 0; i--)
+            {
+                Console.WriteLine($"Starting in {i} second(s)...");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+
+            Console.WriteLine("Starting test.");
+        }
+    }
+}
--- src/hurry/Program.cs
+++ src/hurry/Program.cs
@@ -1,26 +1,34 @@
 ﻿using hurry.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace hurry;
 
 public static class Program
 {
-    private static readonly ITestService _testService =
-        new TestService(new TimerService(), new PromptService(), new ResultsService());
+    private static async Task Main(string[] args)
+    {
+        var services = new ServiceCollection()
+            .AddSingleton<ITestService>(new TestService( new TimerService(), new PromptService(), new ResultsService()));
 
-    private static async Task Main()
+        using var serviceProvider = services.BuildServiceProvider();
+        ITestService testService = serviceProvider.GetService<ITestService>()!;
+
+        ConsoleHelper.Greet();
+        await ConsoleHelper.StartCountdown();
+        
+        var wpm = RunTest(testService).Result;
+        Console.WriteLine($"You're WPM is {wpm}.");
+    }
+
+    static async Task<int> RunTest(ITestService testService)
     {
         var cts = new CancellationTokenSource();
         var token = cts.Token;
 
-        Console.WriteLine("Welcome to hurry, a typing test where you want to hurry.");
-        Console.WriteLine("You will be given one minute to type out the prompt that you will see");
-        Console.WriteLine("If you want to end the program early, you can type 'quit'");
-        await StartCountdown();
-
-        var prompt = _testService.GetPrompt();
+        var prompt = testService.GetPrompt();
         Console.WriteLine(prompt);
 
-        var taskStart = _testService.Start(token);
+        var taskStart = testService.Start(token);
         var tmpInput = new List<string>();
         var input = "";
         try
@@ -28,7 +36,7 @@ public static class Program
             while (!taskStart.IsCompleted)
             {
                 tmpInput.Add(Console.ReadLine()!);
-                
+
                 if (tmpInput.Last().ToLower() == "quit")
                 {
                     cts.Cancel();
@@ -48,19 +56,9 @@ public static class Program
             Console.WriteLine($"Error: {e.Message}");
         }
 
-        _testService.Stop(input);
-        var results = _testService.GetResults();
-        Console.WriteLine($"You're WPM is {results!.Wpm}.");
-    }
+        testService.Stop(input);
+        var results = testService.GetResults();
 
-    private static async Task StartCountdown()
-    {
-        for (var i = 3; i > 0; i--)
-        {
-            Console.WriteLine($"Starting in {i} second(s)...");
-            await Task.Delay(TimeSpan.FromSeconds(1));
-        }
-
-        Console.WriteLine("Starting test.");
+        return results!.Wpm;
     }
 }
