a3c3f5e71a7dfb62963dd3601680ae9e6b955c2f
Devin Harris <49620674+devinstormharris@users.noreply.github.com>
2022-03-06 17:01:40 -0500

    Replaced service with helper

---
+++ src/Hurry.Utilities/Helpers/PromptHelper.cs
@@ -0,0 +1,28 @@
+namespace Hurry.Utilities.Helpers;
+
+using System.Reflection;
+
+public static class PromptHelper
+{
+    public static string GetPrompt()
+    {
+        var prompts = new Dictionary<int, string>
+        {
+            {0, "prompt1.txt"},
+            {1, "prompt2.txt"}
+        };
+
+        var random = new Random();
+        var randomInt = random.Next(prompts.Count);
+
+        var assembly = Assembly.GetExecutingAssembly();
+        var promptPath = assembly.GetManifestResourceNames()
+            .Single(str => str.EndsWith(prompts[randomInt]));
+
+        using var stream = assembly.GetManifestResourceStream(promptPath);
+        using var reader = new StreamReader(stream!);
+        var prompt = reader.ReadToEnd();
+
+        return prompt;
+    }
+}
