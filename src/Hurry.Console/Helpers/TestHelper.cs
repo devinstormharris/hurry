c66e4d4543f615412f3d19c49c7ae8633940ea3f
Devin Harris <49620674+devinstormharris@users.noreply.github.com>
2022-03-08 21:05:16 -0500

    Code organization

--- src/Hurry.Console/Helpers/TestHelper.cs
+++ src/Hurry.Console/Helpers/TestHelper.cs
@@ -7,10 +7,10 @@ public static class TestHelper
     public static async Task RunTest(this TestService testService)
     {
         ConsoleHelper.WaitForActivity();
-        
+
         await ConsoleHelper.StartCountdown();
         ConsoleHelper.WritePrompt(testService.Test.Prompt);
-        
+
         _ = testService.StartTimer();
 
         var input = ConsoleHelper.GetUserInput();
