5fbc03389670ba045943e77297f0f5d0a41ee82c
Devin Harris <49620674+devinstormharris@users.noreply.github.com>
2022-03-14 20:12:27 -0400

    Create AccuracyService.cs

---
+++ src/Hurry.Utilities/Services/AccuracyService.cs
@@ -0,0 +1,12 @@
+using Hurry.Utilities.Models;
+
+namespace Hurry.Utilities.Services;
+
+public class AccuracyService
+{
+    public Test CalculateAccuracy(Test test)
+    {
+
+        return test;
+    }
+}
