a1216db0f64a3bdbd6e5de60841808cc39abb952
Devin Harris <49620674+devinstormharris@users.noreply.github.com>
2022-03-15 16:39:05 -0400

    Merge accuracy & WPM services into ResultService.cs

--- src/Hurry.Utilities/Services/ResultService.cs
+++ src/Hurry.Utilities/Services/ResultService.cs
@@ -2,19 +2,45 @@ using Hurry.Utilities.Models;
 
 namespace Hurry.Utilities.Services;
 
-public class WpmService
+public class ResultService
 {
-    public Test CalculateWpm(Test test)
+    public Test GetWpm(Test test)
     {
+        GetErrors(test);
         var wordCount = (test.UserInput.Length / 5) - test.Result.Errors;
-        var time = CalculateMinutes(test);
+        var time = GetMinutes(test);
 
         test.Result!.Wpm = (int) (wordCount / time);
 
         return test;
     }
 
-    private static double CalculateMinutes(Test test)
+    private Test GetErrors(Test test)
+    {
+        var accuracy = 1.0;
+
+        var prompt = test.Prompt.Split();
+        var userInput = test.UserInput.Split();
+
+        for (int i = 0; i < userInput.Length; i++)
+        {
+            if (prompt[i] != userInput[i])
+            {
+                test.Result.Errors++;
+            }
+        }
+
+        if (test.Result.Errors > 0)
+        {
+            accuracy = 1 - Math.Round((double)test.Result.Errors / userInput.Length, 1);
+        }
+
+        test.Result.Accuracy = accuracy;
+
+        return test;
+    }
+    
+    private static double GetMinutes(Test test)
     {
         var minutes = 0.0;
         var seconds = test.Result.SecondsElapsed;
