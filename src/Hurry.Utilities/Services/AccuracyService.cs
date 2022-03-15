0e16520a83610608ff6734bc19a27e0ee9d6a195
Devin Harris <49620674+devinstormharris@users.noreply.github.com>
2022-03-14 21:00:14 -0400

    Create CalculateAccuracy logic in AccuracyService.cs

--- src/Hurry.Utilities/Services/AccuracyService.cs
+++ src/Hurry.Utilities/Services/AccuracyService.cs
@@ -6,7 +6,27 @@ public class AccuracyService
 {
     public Test CalculateAccuracy(Test test)
     {
+        var accuracy = 1.0;
+        var errors = 0.0;
 
+        var prompt = test.Prompt.Split();
+        var userInput = test.UserInput.Split();
+
+        for (int i = 0; i < userInput.Length; i++)
+        {
+            if (prompt[i] != userInput[i])
+            {
+                errors++;
+            }
+        }
+
+        if (errors > 0)
+        {
+            accuracy = 1 - Math.Round(errors / userInput.Length, 1);
+        }
+
+        test.Result.Accuracy = accuracy;
+        
         return test;
     }
 }
